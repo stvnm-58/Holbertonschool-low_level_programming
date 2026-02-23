9db026d6f1bea16b8c8cb43bf5ee4e443fbd3928
steven <12992@holbertonstudents.com>
2026-02-23 11:33:03 +0000

    correction

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /*Commentaire 1*/
-int main (void)
+int main(void)
 /*Commentaire 2*/
 
-
 {
-    puts ("\"Programming is like building a multilingual puzzle");
-return 0;
-
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
