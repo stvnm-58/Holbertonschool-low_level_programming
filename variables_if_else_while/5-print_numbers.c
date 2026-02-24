413c210d39d5a5aa167f6097271208b3a1fddafc
steven <12992@holbertonstudents.com>
2026-02-24 12:25:29 +0000

    correction

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 0;
+
+while (n < 10)
+{
+printf("%d", n);
+n = n + 1;
+}
+printf("\n");
+
+return (0);
+
+}
