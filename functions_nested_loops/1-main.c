86232ec13e06ac0f0be8a3148e02ec0b7a2afee1
steven <12992@holbertonstudents.com>
2026-02-25 14:23:59 +0000

    correction

---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Point d'entrée du programme
+ *
+ * Return: Toujours 0 (succès)
+ */
+ int main(void)
+ {
+print_alphabet();
+return (0);
+ }
---
+++ functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+#include "1-main.h"
+/**
+ * main - Point d'entrée du programme
+ *
+ * Return: Toujours 0 (succès)
+ */
+ int main(void)
+ {
+print_alphabet();
+return (0);
+ }
