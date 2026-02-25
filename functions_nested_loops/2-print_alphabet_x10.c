f72cf3ae8560dabb9473f890609d7631984e065f
steven <12992@holbertonstudents.com>
2026-02-25 13:52:57 +0000

    correction

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,19 +5,19 @@
 void print_alphabet_x10(void)
 
 {
-    int i = 0;
-    char l;
+int i = 0;
+char l;
 
-    while (i < 10)
-    {
-        l = 'a'; 
+while (i < 10)
+{
+l = 'a'; 
         
-        while (l <= 'z')
-        {
-            _putchar(l);
-            l++;
-        }
-        _putchar('\n');
-        i++;
-    }
+while (l <= 'z')
+{
+_putchar(l);
+l++;
+}
+_putchar('\n');
+i++;
+}
 }
