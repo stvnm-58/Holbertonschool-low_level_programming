77733add8e0f6f9a8656f4f07e5db4bfdd335b25
steven <12992@holbertonstudents.com>
2026-02-25 13:56:06 +0000

    correction

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* main - check the code.
-*
-* Return: Always 0.
-*/
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: void
+ */
 void print_alphabet_x10(void)
 
 {
