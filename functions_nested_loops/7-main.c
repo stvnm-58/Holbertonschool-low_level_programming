11671a09d77af0aec7ce396f0e068e8701dc2d2c
steven <12992@holbertonstudents.com>
2026-02-26 08:55:35 +0000

    _last digit

--- functions_nested_loops/7-main.c
+++ functions_nested_loops/7-main.c
@@ -1,15 +1,18 @@
-include "main.h"
+#include "main.h"
+
 /**
-* print_last_digit - prints the last digit of a number
-* @n: the int to extract the last digit from
+* main - check the code
 *
-* Return: value of the last digit
+* Return: Always 0.
 */
-int print_last_digit(int n)
+int main(void)
 {
-int last_digit;
+int r;
 
-last_digit = n % 10;
-return (last_digit)
-
+print_last_digit(98);
+print_last_digit(0);
+r = print_last_digit(-1024);
+_putchar('0' + r);
+_putchar('\n');
+return (0);
 }
--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,15 @@
-#include "main.h"
-
+include "main.h"
 /**
-* main - check the code
+* print_last_digit - prints the last digit of a number
+* @n: the int to extract the last digit from
 *
-* Return: Always 0.
+* Return: value of the last digit
 */
-int main(void)
+int print_last_digit(int n)
 {
-int r;
+int last_digit;
 
-print_last_digit(98);
-print_last_digit(0);
-r = print_last_digit(-1024);
-_putchar('0' + r);
-_putchar('\n');
-return (0);
+last_digit = n % 10;
+return (last_digit)
+
 }
