8efd034471fce69b68470d43f92ecc02a1d915d0
steven <12992@holbertonstudents.com>
2026-02-26 12:13:04 +0000

    time tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0
- *
- * Return: void
- */
+* times_table - prints the 9 times table, starting with 0
+*
+* Return: void
+*/
 void times_table(void)
 {
 int i, j, res;
