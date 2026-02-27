f0ce61bcc7374807e12d254aa77630b66cf07fcb
steven <12992@holbertonstudents.com>
2026-02-27 12:11:21 +0000

    more numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* more_numbers - prints 10 times the numbers, from 0 to 14,
+* followed by a new line
+*/
+void more_numbers(void)
+{
+int a = 0;
+int b = 0;
+
+	while (a <= 10)
+	{
+		while (b <= 14)
+		{
+		_putchar(b);
+		b++;
+		}
+
+	_putchar('\n');
+	a++;
+	}
+
+}
