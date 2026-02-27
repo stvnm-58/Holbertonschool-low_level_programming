2e1794cecc8f0856071a2e67ffded7731f1a4d16
steven <12992@holbertonstudents.com>
2026-02-27 10:20:51 +0000

    print number

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -4,14 +4,14 @@
 */
 void print_numbers (void)
 {
-    int a = 0;
+	int a = 0;
 
-    while (a <= 9)
-    {
-        _putchar(a + '0');
-        a++;
-    }
-    
+	while (a <= 9)
+	{
+		_putchar(a + '0');
+		a++;
+	}
+
 _putchar ('\n');
 
 }
