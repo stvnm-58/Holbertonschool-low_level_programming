e0cef0f54a528c8d5c7575f835d2aca298d4ee91
steven <12992@hollbertonstudents.com>
2026-03-02 10:04:07 +0000

    Invalid choice

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -11,13 +11,23 @@ int main(void)
 {
 int choice;
 
+while (choice != 0, 1, 2, 3, 4)
+{
 printf("1) Add\n2) Substract\n3) Multiply\n4) Divide\n0) Quit\n");
 printf("Choice: ");
 scanf("%d", &choice);
 
-if (choice == 0)
-{
-	printf ("Bye!\n");
+	if (choice > 4)
+	{
+	printf("Invalid choice\n");
+	}
+
+	if (choice == 0)
+	{
+		printf ("Bye!\n");
+		return (0);
+	}
+
 }
 
 return (0);
