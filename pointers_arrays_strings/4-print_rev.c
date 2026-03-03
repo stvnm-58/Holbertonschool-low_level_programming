0339bc60e1308db23bc4aa59fb86c7f77e6e63ff
steven <12992@holbertonstudents.com>
2026-03-03 09:13:53 +0000

    print reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* rev_string - reverses a string in place
+* @s: the string to be reversed
+*/
+void print_rev(char *s)
+
+{
+int i = 0;
+
+while (i != '\0')
+{
+i++;
+}
+
+i--
+
+while (i >= 0)
+{
+	_putchar(str[i]);
+	i--;
+}
+
+
+_putchar('\n');
+}
