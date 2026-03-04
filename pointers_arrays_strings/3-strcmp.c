fd6753de1e98111dc1fae4398be6e15a2509f164
steven <12992@holbertonstudents.com>
2026-03-04 11:13:39 +0000

    correction

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,21 @@
 #include "main.h"
-
+/**
+* _strcmp - Compares two strings
+* @s1: The first string
+* @s2: The second string
+*
+* Return: The difference
+*/
 int _strcmp(char *s1, char *s2)
 {
-int i = 0; 
+	int i = 0;
 
-while (s1[i] == s2[i])
-/*la boucle avance et ne s'arrête que lorsque s1 et s2 sont identique*/
-{
- i++;
-}
+	while (s1[i] != '\0' && s1[i] == s2[i])
+	/*la boucle avance et ne s'arrête que lorsque s1 et s2 sont identique*/
+	{
+		i++;
+	}
 
-
-return (s1[i] - s2[i])
-/*retourne le caractère qui a été trouvé différent*/
+	return (s1[i] - s2[i]);
+	/*retourne le caractère qui a été trouvé différent*/
 }
