34cef78a969e5ca79225b377c6ce4b34389de215
steven <12992@holbertonstudents.com>
2026-03-04 11:15:17 +0000

    correction

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,21 +7,23 @@
 */
 void print_rev(char *s)
 {
-int i = 0;
-
-while (s[i] != '\0')
-
-{
-i++;
-}
-
-i--;
+	int i = 0;
 
 
-while (i >= 0)
-{
-_putchar(s[i]);
-i--;
-}
-_putchar('\n');
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+
+	i--;
+
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+
+	_putchar('\n');
 }
