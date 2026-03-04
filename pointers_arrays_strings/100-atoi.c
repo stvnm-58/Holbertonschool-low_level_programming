e11585316e68e7e953d64671290020b2a7acbd6b
steven <12992@holbertonstudents.com>
2026-03-04 13:54:55 +0000

    correction

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,19 +12,19 @@ int _atoi(char *s)
 	unsigned int resultat = 0;
 
 
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))/*boucle tant qu'on est pas sur NULL ET qu'on a pas croisé de chiffre*/
 	{
-		if (s[i] == '-')
+		if (s[i] == '-')/* si tiret*/
 		{
-			signe = signe * -1;
+			signe = signe * -1;/* rend imper nega' et pair posi'*/
 		}
 		i++;
 	}
 
 
-	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))/*boucle tant qu'on est pas sur NULL et qu'on est sur un chiffre*/
 	{
-		resultat = (resultat * 10) + (s[i] - '0');
+		resultat = (resultat * 10) + (s[i] - '0');/*multiplie résultat (de base à 0) par 10 pour décaler unité vers dizaine puis soustraction apr le '0' (48 en ASCII)*/
 		i++;
 	}
 
