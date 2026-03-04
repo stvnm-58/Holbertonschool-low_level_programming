20454fa4f6bd0ba000bba7b4d1c255c82f0eb281
steven <12992@holbertonstudents.com>
2026-03-04 11:16:27 +0000

    correction

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -6,20 +6,18 @@
 */
 void reverse_array(int *a, int n)
 {
-int i = 0; /*décompte du début*/
-int j = n - 1; /*décompte de la fin*/
-int tmp; /*mon stockage temporaire*/
+	int i = 0; /*décompte du début*/
+	int j = n - 1; /*décompte de la fin*/
+	int tmp; /*mon stockage temporaire*/
 
-while (i < n)
-{
-	tmp = a[i]; /*stock dans temporaire*/
-	a[i] = a[j]; /*attribu la fin à a actuel*/
-	a[j] = tmp; /*récupère ce qu'il y a dans temporaire*/
+	while (i < j)
+	{
+		tmp = a[i]; /*stock dans temporaire*/
+		a[i] = a[j]; /*attribu la fin à a actuel*/
+		a[j] = tmp; /*récupère ce qu'il y a dans temporaire*/
 
-	i++
-	j--
-/*continu le process jusqu'à arriver au milieu*/
-}
-
-
+		i++;
+		j--;
+		/*continu le process jusqu'à arriver au milieu*/
+	}
 }
