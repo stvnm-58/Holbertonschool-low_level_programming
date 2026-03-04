7d088e0829888dc8461f81a91bb9826368788082
steven <12992@holbertonstudents.com>
2026-03-04 09:14:25 +0000

    strncat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,16 @@
 #include "main.h"
-#include <string.h>
-
+/**
+* _strcat - Concatenates two strings
+* @dest: The destination string to be appended to
+* @src: The source string to append
+*
+* Return: A pointer to the resulting string dest
+*/
 char *_strcat(char *dest, char *src)
 
 {
-	int i = 0;
-	int j = 0;
+	int i = 0; /*compteur pour destination*/
+	int j = 0; /*compteur pour source*/
 
 	while (dest[i] != '\0')
 {
---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* _strncat - Concatenates two strings using at most n bytes from src
+* @dest: The destination string to be appended to
+* @src: The source string to append
+* @n: The maximum number of bytes to copy from src
+*
+* Return: A pointer to the resulting string dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0; /*compteur pour destination*/
+int j = 0; /*compteur pour source*/
+
+while (dest[i] != '\0')
+{
+	i++;
+}
+
+while ((j < n) && (src[j] != '\0')) 
+/**
+* boucle tant que j est plus petit que n et src[j] ne rencontre pas '\0
+*/
+{
+	dest[i] = src[j];
+	i++;
+	j++;
+
+}
+
+dest[i] = '\0';
+
+return (dest);
+
+}
