ab3c6859399934faad28cf23904e029cb0b87437
steven <12992@holbertonstudents.com>
2026-03-04 12:02:16 +0000

    correction

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -7,20 +7,16 @@
 */
 char *string_toupper(char *str)
 {
-int i = 0;
+	int i = 0;
 
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
 
-while (str[i] != '\0')
-{
-		
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 32;
-}
-
-i++;
-}
-
-
-return (str);
+	return (str);
 }
