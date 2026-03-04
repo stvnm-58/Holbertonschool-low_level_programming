d0ac5c026147a6d150f10d737c70d969499eba21
steven <12992@holbertonstudents.com>
2026-03-04 10:47:23 +0000

    README.md

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdio.h>
 int _putchar (char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -17,4 +19,4 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 
-#include <unistd.h>
+#endif
