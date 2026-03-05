d5f9016f09bfc75370bf8edacf1d64b34e4d123c
steven <12992@holbertonstudents.com>
2026-03-05 10:23:02 +0000

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,15 @@
 #include "main.h"
-
+/**
+* _strchr - localise un caractère dans une chaîne
+* @s: la chaîne de caractères à parcourir
+* @c: le caractère à rechercher
+*
+* Return: un pointeur vers la première occurrence de c,
+* ou NULL si le caractère n'est pas trouvé
+*/
 char *_strchr(char *s, char c)
 {
-int i = 0; 
+int i = 0;
 
 while (s[i] >= '\0')
 {
