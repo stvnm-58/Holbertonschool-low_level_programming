2d56fa3744ec5f2740a07f35ec9155e56654c3fd
steven <12992@holbertonstudents.com>
2026-03-05 12:22:06 +0000

    strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -8,23 +8,20 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
-	int j;
-
-	while (s[i] != '\0')/*scan *s jusqu'à NULL*/
-	{
-		j = 0;
-		while (accept[j] != '\0')
-		{
-			if (s[i] == accept[j])/*si concordance entre s[i] et accept, retourne le caractère correspondant*/
-			{
-				return(&s[i]);
-			}
-
-			j++;
-		}
-		i++;
-	}
-
-	return ('\0');/*retourne NULL si rien*/
+int i = 0;
+int j;
+while (s[i] != '\0')/*scan *s jusqu'à NULL*/
+{
+j = 0;
+while (accept[j] != '\0')
+{
+if (s[i] == accept[j])/*si concordance entre s[i] et accept, retourne le caractère correspondant*/
+{
+return(&s[i]);
+}
+j++;
+}
+i++;
+}
+return ('\0');/*retourne NULL si rien*/
 }
