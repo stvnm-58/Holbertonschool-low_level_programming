ea08e28d574143f84e1192e82240358754c27fb3
steven <12992@holbertonstudents.com>
2026-03-05 12:06:31 +0000

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,34 +8,29 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0;
-	unsigned int j;
-	unsigned int count = 0;
-	int found;
-
-	while (s[i] != '\0')
-	{
-		found = 0;
-		j = 0;
-
-		while (accept[j] != '\0')
-		{
-			if (s[i] == accept[j])
-			{
-				found = 1;
-				count++;
-				break; 
-			}
-			j++;
-		}
-
-		if (found == 0)
-		{
-			return (count);
-		}
-
-		i++;
-	}
-
-	return (count);
+unsigned int i = 0;
+unsigned int j;
+unsigned int count = 0;
+int found;
+while (s[i] != '\0')
+{
+found = 0;
+j = 0;
+while (accept[j] != '\0')
+{
+if (s[i] == accept[j])
+{
+found = 1;
+count++;
+break; 
+}
+j++;
+}
+if (found == 0)
+{
+return (count);
+}
+i++;
+}
+return (count);
 }
