4f9ff29375aefb7da605c8644386bd54e44658b5
steven <12992@holbertonstudents.com>
2026-03-05 10:19:26 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+char *_strchr(char *s, char c)
+{
+int i = 0; 
+
+while (s[i] >= '\0')
+{
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+
+	if(s[i] == '\0')
+	{
+		break;
+	}
+
+i++;
+}
+
+return ('\0');
+
+}
