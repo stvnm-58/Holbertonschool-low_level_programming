5b46bde39e92d52d2d5349dd22441fa4862fc3fe
steven <12992@holbertonstudents.com>
2026-03-05 13:01:09 +0000

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -6,20 +6,30 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-int i = 0;
-int j;
-if (*needle == '\0')
-return (haystack);
-while (haystack[i] != '\0')
-{
-j = 0;
-while (haystack[i + j] == needle[j])
-{
-if (needle[j + 1] == '\0')
-return (&haystack[i]);
-j++;
-}
-i++;
-}
-return ('\0');
+	int i = 0;	/* pour scan haystack */
+	int j;		/* pour scan needle */
+
+	if (*needle == '\0')
+		return (haystack);
+
+
+	while (haystack[i] != '\0')/*scan haystack*/
+	{
+		j = 0;/* remise à 0 */
+
+		/* tant qu'il y a correspondance haystack, needle on continue */
+		while (haystack[i + j] == needle[j])
+		{
+			/* Si le prochain caractère de needle est la fin (\0)...*/
+			if (needle[j + 1] == '\0')
+				/* on renvoi l'adresse haystack correspondant i */
+				return (&haystack[i]);
+
+			j++;/* carac suivant */
+		}
+		
+		i++;/* carac suivant du haystack */
+	}
+
+	return ('\0');/* si echec alors NULL */
 }
