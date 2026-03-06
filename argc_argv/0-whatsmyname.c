660cf6119c3ad8f3cfba543cfb2914ebf5c8b5fa
steven <12992@holbertonstudents.com>
2026-03-06 12:01:17 +0000

    correction

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -10,9 +10,11 @@ int main(int argc, char *argv[])
 {
 
 int i = 0;
-(void)argc;/*sans ça, le compilateur bloque car argc n'est pas "utilisé"*/
+(void)argc;
+/*sans ça, le compilateur bloque car argc n'est pas "utilisé"*/
 
-while (argv[0][i] != '\0')/*argc[0] correspond au nom. Et [i] le caractère qui compose ce nom*/
+while (argv[0][i] != '\0')
+/*argc[0] correspond au nom. Et [i] le caractère qui compose ce nom*/
 {
 	_putchar(argv[0][i]);
 	i++;
