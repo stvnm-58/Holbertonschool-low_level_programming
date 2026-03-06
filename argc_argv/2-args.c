8d72920a404844f27b0daac7497562ff1b7768cc
steven <12992@holbertonstudents.com>
2026-03-06 13:27:33 +0000

    args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - prints the number of arguments passed into it
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+
+(void)argv;
+/*toujours pour exclure argc de la compil'*/
+
+_putchar(argc + '0');
+
+_putchar('\n');
+
+return (0);
+
+}
