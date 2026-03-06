e7270e55a023bd6fd6cbefe9eff7d8ea431fd10b
steven <12992@holbertonstudents.com>
2026-03-06 15:17:45 +0000

    correction

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,9 +1,11 @@
 #include "main.h"
-
-
-
-
-
+/**
+* main - prints all arguments it receives
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0
+*/
 int main(int argc, char *argv[])
 {
 
