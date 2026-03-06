7173a5616dacad0a831685100615e02b251251ce
steven <12992@holbertonstudents.com>
2026-03-06 11:59:46 +0000

    correction

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,18 @@
 #include "main.h"
-
-int main(char *argv[])
+/**
+* main - prints its name, followed by a new line.
+* @argc: number of arguments.
+* @argv: array of arguments.
+*
+* Return: Always 0 (Success).
+*/
+int main(int argc, char *argv[])
 {
 
 int i = 0;
+(void)argc;/*sans ça, le compilateur bloque car argc n'est pas "utilisé"*/
 
-while (argv[0][i] != '\0')
+while (argv[0][i] != '\0')/*argc[0] correspond au nom. Et [i] le caractère qui compose ce nom*/
 {
 	_putchar(argv[0][i]);
 	i++;
