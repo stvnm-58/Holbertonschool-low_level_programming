b21ecb97e2aa79a6e8923176aa526393163c44f3
steven <12992@holbertonstudents.com>
2026-03-06 10:55:58 +0000

    chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_chessboard - prints the chessboard
+* @a: pointer to the 2D array representing the board
+*
+* Return: void
+*/
+void print_chessboard(char (*a)[8])
+{
+int i = 0 /*ligne*/
+int j; /*colonne*/
+
+while (i < 8)/*défile les lignes*/
+{
+
+	for (j = 0;  j < 8 ; j++)
+	{
+		_putchar(a[i][j]);/*écrit ce qui correspond dans la colonne j*/
+		
+
+	}
+}
+
+i++/*passe à la ligne suivante*/
+
+_putchar('\n');
+
+}
