6ded5149cbec3a8ca97abe07a59da93df95ae242
steven <12992@holbertonstudents.com>
2026-03-08 14:35:01 +0000

    mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,82 +1,42 @@
-#include "main.h"
-
-/**
-* main - multiplie deux nombres passés en arguments
-* @argc: nombre d'arguments reçus
-* @argv: tableau de chaînes de caractères (les arguments)
-*
-* Return: 0 si succès, 1 si erreur (mauvais nombre d'arguments)
-*/
 int main(int argc, char *argv[])
 {
-
-	int i;
-	int num1 = 0;
-	int num2 = 0;
-	int resultat;
-	int div;
+	int i, j, n1 = 0, n2 = 0, res, div = 1;
 
 	if (argc != 3)
 	/*Bloc error*/
 	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-
+		_putchar('E'), _putchar('r'), _putchar('r'), _putchar('o'), _putchar('r'), _putchar('\n');
 		return (1);
 	}
-
-	/* --- TA LOGIQUE DE CONVERSION --- */
-	for (i = 0; argv[1][i] != '\0'; i++)
 	/* conversion argv[1] en entier */
-	{
-		if (argv[1][i] == '-') continue;
-		/*ignore le signe -*/
-		num1 = (num1 * 10) + (argv[1][i] - '0');
-	}
-	if (argv[1][0] == '-') num1 = -num1;
-
-	for (i = 0; argv[2][i] != '\0'; i++)
+	for (i = (argv[1][0] == '-') ? 1 : 0; argv[1][i] != '\0'; i++)
+		n1 = (n1 * 10) + (argv[1][i] - '0');
+	if (argv[1][0] == '-')
+		n1 = -n1;
 	/* conversion argv[2] en entier */
-	{
-		if (argv[2][i] == '-') continue;
-		num2 = (num2 * 10) + (argv[2][i] - '0');
-	}
-	if (argv[2][0] == '-') num2 = -num2;
-
-	resultat = num1 * num2;
-
-	if (resultat == 0)
-	{
+	for (j = (argv[2][0] == '-') ? 1 : 0; argv[2][j] != '\0'; j++)
+		n2 = (n2 * 10) + (argv[2][j] - '0');
+	if (argv[2][0] == '-')
+		n2 = -n2;
+	res = n1 * n2;
+	if (res == 0)
 		_putchar('0');
-	}
 	else
 	{
-		if (resultat < 0)
+		if (res < 0)
 		/*affichage des négatifs*/
 		{
-			_putchar('-');
-			resultat = -resultat;
+			_putchar('-'), res = -res;
 		}
-
-		div = 1;
-		while (div <= resultat / 10)
+		while (div <= res / 10)
 		/*x10 pour à chaque tourne pour print centaine, dizaine, unité etc etc.*/
-		{
 			div *= 10;
-		}
-
-		/* Affichage des chiffres */
 		while (div > 0)
+		/* Affichage des chiffres */
 		{
-			_putchar(((resultat / div) % 10) + '0');
-			div /= 10;
+			_putchar(((res / div) % 10) + '0'), div /= 10;
 		}
 	}
 	_putchar('\n');
-
 	return (0);
 }
