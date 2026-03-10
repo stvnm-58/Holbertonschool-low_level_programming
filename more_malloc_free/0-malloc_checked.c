a6b7e0169336b9163fa5f59550bc2c0cc9ec8786
steven <12992@holbertonstudents.com>
2026-03-10 10:06:48 +0000

    Check

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+* malloc_checked - alloue de la mémoire avec malloc
+* @b: nombre d'octets à allouer
+*
+* Return: un pointeur vers la mémoire allouée
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	/*Alloue b de place à ptr*/
+	
+	if (ptr == NULL)
+	{
+		exit(98);
+		/*kill le programme si null, renvoie 98*/
+	}
+
+return (ptr);
+/*return ptr si ok*/
+
+}
