281318fe543b66ba1c34bcdb860b470e048f0518
steven <12992@holbertonstudents.com>
2026-03-11 10:27:46 +0000

    dog fonction

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+* print_dog - affiche les informations d'une structure dog
+* @d: pointeur vers la structure dog à afficher
+*
+* Description: Si d est NULL, la fonction ne fait rien.
+* Si un élément de la structure est NULL, affiche (nil) à sa place.
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+if (d != '\0')
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
+}
