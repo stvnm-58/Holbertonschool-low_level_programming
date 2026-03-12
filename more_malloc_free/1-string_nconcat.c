a11924cec2b2e93c414af725ae2a06378188f430
steven <12992@holbertonstudents.com>
2026-03-12 09:48:43 +0000

    correction

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,32 +1,50 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: bytes to include from s2
- * Return: pointer to new string, or NULL
- */
+* nom_de_la_fonction - Description courte de ce qu'elle fait
+* @parametre1: Description du premier argument
+* @parametre2: Description du second argument
+*
+* Return: Description de la valeur de retour (ex: 0 en cas de succès)
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *res;
-	unsigned int i = 0, i2 = 0, j, j2;
+
+char *result;
+unsigned int i = 0, i2 = 0, j = 0, j2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[i2] && i2 < n)
-		i2++;
-	res = malloc(sizeof(char) * (i + i2 + 1));
-	if (res == NULL)
-		return (NULL);
-	for (j = 0; j < i; j++)
-		res[j] = s1[j];
-	for (j2 = 0; j2 < i2; j++, j2++)
-		res[j] = s2[j2];
-	res[j] = '\0';
-	return (res);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	;
+
+	for (i2 = 0; s2[i2] != '\0' && i2 < n; i2++)
+	/*c'est ici qu'on limite le comptage à \0 ET < n*/
+	;
+
+result = malloc (sizeof(char) * i + i2 + 1);
+if (result == NULL)
+    {
+	return (NULL);
+	}
+
+
+for (j = 0; s1[j] != '\0' ; j++ )
+{
+	result[j] = s1[j];
+}
+
+for (j2 = 0; s2[j2] != '\0' && j2 < n ; j++, j2++)
+/*même chose que précédemment pour limiter le nombre d'inscription*/
+{
+	result[j] = s2[j2];
+}
+	result[j] = '\0';
+
+return (result);
+
 }
