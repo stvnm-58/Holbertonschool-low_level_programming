6bb107c90a16ca534412f78cca713bb6847c5f5a
steven <12992@holbertonstudents.com>
2026-03-12 12:18:11 +0000

    free

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+* free_dog - frees dogs
+* @d: pointer to the dog to free
+*/
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+	free(d->name);
+	free(d->age);
+	free(d);
+}
+
+}
