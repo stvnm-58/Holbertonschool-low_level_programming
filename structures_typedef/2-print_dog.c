0b717dfaff9781d47426b40efa29500a52ce8548
steven <12992@holbertonstudents.com>
2026-03-12 12:00:02 +0000

    correction

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,12 +1,15 @@
 #include "dog.h"
 #include <stdio.h>
-
+/**
+* print_dog - prints a struct dog
+* @d: pointer to struct dog to print
+*/
 void print_dog(struct dog *d)
 {
 
-if (d != '\0')
+if (d != NULL)
 {
-if (d->name == '\0')
+if (d->name == NULL)
 {
 	printf("Name: (nil)\n");
 	}
@@ -17,7 +20,7 @@ printf("Name: %s\n", d->name);
 
 printf("Age: %f\n", d->age);
 
-	if (d->owner == '\0')
+	if (d->owner == NULL)
 {
 	printf("Owner: (nil)\n");
 	}
