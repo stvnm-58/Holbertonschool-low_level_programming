37592e851bd6765c6e243bd8138e1032fa234325
steven <12992@holbertonstudents.com>
2026-03-12 09:16:47 +0000

    correction

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * str_concat - concatenates two strings
  * @s1: the first string
@@ -13,12 +12,10 @@ char *str_concat(char *s1, char *s2)
 {
 	int i, i2, j, j2;
 	char *result;
-
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	i = 0;
 	i2 = 0;
 	j = 0;
@@ -29,29 +26,23 @@ char *str_concat(char *s1, char *s2)
 	for (i2 = 0; s2[i2] != '\0'; i2++)
 		;
 	/*permet de compter l'espace que prend s1 + s2 sans \0*/
-
 	result = malloc(sizeof(char) * (i + i2 + 1));
 	/*permet d'avoir la taille de s1 + s2 + \0*/
-
 	if (result == NULL)
 	/*retour NULL de sécurité */
 	{
 		return (NULL);
 	}
-
 	for (j = 0; s1[j] != '\0'; j++)
 	/*copiage dans result*/
 	{
 		result[j] = s1[j];
 	}
-
 	for (j2 = 0; s2[j2] != '\0'; j++, j2++)
 	/*copiage de de la suite dans result*/
 	{
 		result[j] = s2[j2];
 	}
-
 	result[j] = '\0';
-
 	return (result);
 }
