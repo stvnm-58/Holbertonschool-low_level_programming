c453f230a1c0ce9e9a68be762b5f60b4b959bceb
steven <12992@holbertonstudents.com>
2026-03-12 09:21:35 +0000

    correction

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -12,24 +12,18 @@ int **alloc_grid(int width, int height)
 {
 	int **grid;
 	int i, j;
-
-
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-
 	grid = malloc(sizeof(int *) * height);
-	/*Création du nombre de ligne*/
 	if (grid == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
-		/*création des colonnes*/
 		if (grid[i] == NULL)
 		{
 			for (; i >= 0; i--)
@@ -40,11 +34,9 @@ int **alloc_grid(int width, int height)
 			}
 		}
 		for (j = 0; j < width; j++)
-		/*remplissage du tableau*/
 		{
 			grid[i][j] = 0;
 		}
 	}
-
 	return (grid);
 }
