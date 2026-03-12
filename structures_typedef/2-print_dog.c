946e9dac049da1488cb458d787e2ee6508d316cd
steven <12992@holbertonstudents.com>
2026-03-12 11:57:27 +0000

    correction

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -4,24 +4,18 @@
 void print_dog(struct dog *d)
 {
 
-
 if (d != '\0')
 {
-
 if (d->name == '\0')
 {
 	printf("Name: (nil)\n");
 	}
 		else
 	{
-
 printf("Name: %s\n", d->name);
 }
 
-
 printf("Age: %f\n", d->age);
-
-
 
 	if (d->owner == '\0')
 {
@@ -29,11 +23,7 @@ printf("Age: %f\n", d->age);
 	}
 		else
 	{
-
 printf("Owner: %s\n", d->owner);
 }
-
 }
-
-
 }
