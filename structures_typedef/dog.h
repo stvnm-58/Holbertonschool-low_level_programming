1788bb39967480c8713dbc14cc60f6d639afcd62
steven <12992@holbertonstudents.com>
2026-03-12 11:50:24 +0000

    correction

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,8 @@
-struct dog
+#ifndef DOG_H
+#define DOG_H
+
+
+typedef struct dog
 {
 char *name;
 float age;
@@ -7,3 +11,5 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+#endif
