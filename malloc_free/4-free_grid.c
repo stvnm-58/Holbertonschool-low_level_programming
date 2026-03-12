b28a03ca6fbbc9cff2c3626003fdef762e0c58a6
steven <12992@holbertonstudents.com>
2026-03-12 09:32:13 +0000

    correction

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,19 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * free_grid - frees a 2 dimension grid previously created by alloc_grid
+ * @grid: the 2D array to free
+ * @height: height of the grid
+ */
 void free_grid(int **grid, int height)
 {
-int i = 0
+	int i;
 
-for (i = 0; i < height; i++)
-/*on libère la mémoire de la ligne, i par i*/
-{
-	free(grid[i]);
-}
-/*une fois que tout est clean dans la ligne, on cfree grid*/
-	free(
-		
-	);
+	/* Si la grille est déjà vide, on ne fait rien pour éviter un crash */
+	if (grid == NULL || height <= 0)
+		return;
 
+	for (i = 0; i < height; i++)
+	/* on libère la mémoire de la ligne, i par i */
+	{
+		free(grid[i]);
+	}
+
+	/* une fois que tout est clean dans les lignes, on free le tableau de pointeurs */
+	free(grid);
 }
