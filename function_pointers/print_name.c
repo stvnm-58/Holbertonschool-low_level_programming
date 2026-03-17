ae79000466bdd4c2726e28ce1e6871047c1e9a6d
steven <12992@holbertonstudents.com>
2026-03-17 17:44:41 +0100

    correction

---
+++ function_pointers/print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+* array_iterator - executes a function on each element of an array
+* @array: the array of integers
+* @size: the size of the array
+* @action: pointer to the function to be executed
+*
+* Return: Nothing.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+
+	if (name == NULL || f == NULL)
+	{
+	return;
+	}
+	f(name);
+}
