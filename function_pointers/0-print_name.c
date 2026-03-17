06011747e35f6f648eb8e9f659aed1e3f78e5b77
steven <12992@holbertonstudents.com>
2026-03-17 17:03:07 +0100

    correction

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -7,11 +7,12 @@
 *
 * Return: Nothing.
 */
+
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-{
-return;
-}
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
 f(name);
 }
