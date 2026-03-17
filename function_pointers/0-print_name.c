8ffd2f682eabe7d569514744106a95587eb314c0
steven <12992@holbertonstudents.com>
2026-03-17 17:19:44 +0100

    correction

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,15 +4,13 @@
 * print_name - prints a name
 * @name: name of the person
 * @f: pointer to the printing function
-*
 * Return: Nothing.
 */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 	{
-		return;
+	return;
 	}
 f(name);
 }
