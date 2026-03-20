4f43a0dc11725712d2cedb2b14315cb135264fe9
steven <12992@holbertonstudents.com>
2026-03-20 14:15:43 +0100

    correction

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,8 +11,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list num;
 	unsigned int i;
 
-	va_start (num, n);
-	
+	va_start(num, n);
+
 for (i = 0; i < n ; i++)
 {
 	printf("%d", va_arg(num, int));
