53177b709deecb90920e2eb937797163463032db
steven <12992@holbertonstudents.com>
2026-03-20 12:12:45 +0100

    correction

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,34 +1,32 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * get_op_func - selects the correct function to perform the operation
- * @s: the operator passed as argument
+ * @s: operator passed as argument
  *
  * Return: pointer to the function that corresponds to the operator
  */
 int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	/* MODIFICATION : Boucle de recherche de l'opérateur */
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
 
-while (ops[i].op != NULL)
-{
-
-if (strcmp (ops[i].op, s) == 0 && s[1] == '\0')
-{
-	return (ops[i].f);
-
-}
-	i++;
-}
-
-return (NULL);
-
+	return (NULL);
 }
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -7,44 +7,54 @@
  *
  * Return: sum of a and b
  */
-
-
-/*
-* fonction addition
-*/
 int op_add(int a, int b)
 {
 	return (a + b);
-
 }
 
-/*
-* fonction soustraction
-*/
+/**
+ * op_sub - returns the difference of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
-/*
-*fonction multiplication
-*/
+/**
+ * op_mul - returns the product of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
-/*
-*fonction division
-*/
+/**
+ * op_div - returns the result of the division of a by b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: quotient of a and b
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 
-/*
-*fonction modulo
-*/
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of a divided by b
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
