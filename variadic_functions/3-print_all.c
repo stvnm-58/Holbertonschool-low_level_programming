e2e61c56d1b6c4a3b5561437179563e456b2a990
steven <12992@holbertonstudents.com>
2026-03-20 14:29:17 +0100

    correction

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,86 +1,82 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+
 /**
-* print_all - Prints anything based on the format provided.
-* @format: A list of types of arguments passed to the function.
-*
-* Description: This function iterates through the format string and
-* calls the corresponding function to print the argument.
-*/
-/*
-* En suivant, toutes mes fonctions pour c i f s
-*
-*/
+ * print_char - Imprime un caractère
+ * @liste: Liste des arguments variadiques
+ */
 void print_char(va_list liste)
 {
 	printf("%c", va_arg(liste, int));
 }
 
+/**
+ * print_int - Imprime un entier
+ * @liste: Liste des arguments variadiques
+ */
 void print_int(va_list liste)
 {
 	printf("%d", va_arg(liste, int));
 }
 
+/**
+ * print_float - Imprime un nombre à virgule (float)
+ * @liste: Liste des arguments variadiques
+ */
 void print_float(va_list liste)
 {
 	printf("%f", va_arg(liste, double));
 }
 
+/**
+ * print_string - Imprime une chaîne de caractères
+ * @liste: Liste des arguments variadiques
+ */
 void print_string(va_list liste)
 {
 	char *str = va_arg(liste, char *);
 
 	if (str == NULL)
-	{
 		str = "(nil)";
-	}
-
 	printf("%s", str);
 }
-/**********************************************/
+
+/**
+ * print_all - Imprime n'importe quel type selon le format fourni
+ * @format: Liste des types d'arguments (c, i, f, s)
+ */
 void print_all(const char * const format, ...)
 {
 	va_list liste;
-
-	int i = 0;
-	int j = 0;
-	char *sep = "";
-/*char sep (séparateur)
-*"nul" pour ne pas print lors du bouclage la première fois
-*/
-	format_t lettre[] =
-	{
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_string},
+	int i = 0, j;
+	char *sep = ""; /* Vide au début pour ne pas print de virgule au 1er tour */
+	format_t lettre[] = {
+		{'c', print_char}, {'i', print_int},
+		{'f', print_float}, {'s', print_string},
 		{'\0', NULL}
 	};
 
 	va_start(liste, format);
 
+	/* Boucle tant que le format existe et n'est pas terminé */
 	while (format && format[i])
-	/*
-	* tant que format existe (diff de NULL)
-	* et jusqu'à ce que format[i] devienne NULL
-	*/
 	{
-		j = 0;
-		while (lettre[j].symbole != '\0'){
+		j = 0; /* Remise à zéro pour parcourir le tableau 'lettre' à chaque fois */
+		while (lettre[j].symbole != '\0')
+		{
 			if (format[i] == lettre[j].symbole)
 			{
 				printf("%s", sep);
 				lettre[j].fonction(liste);
-				sep = ", ";
+				sep = ", "; /* Devient une virgule après le 1er élément imprimé */
 				break;
 			}
 			j++;
 		}
-
 		i++;
 	}
-	/* ici les appels des différente fonction dépendamment du type d'entrée*/
+
 	va_end(liste);
 	printf("\n");
 }
