92a0800f356b34f88ef2f6bb370c336b8e718c21
steven <12992@holbertonstudents.com>
2026-03-20 14:10:51 +0100

    correction

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -10,7 +10,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list num;
 	unsigned int i;
-	va_start(num, n);
+	va_start (num, n);
 
 
 for (i = 0; i < n ; i++)
@@ -19,7 +19,7 @@ for (i = 0; i < n ; i++)
 
 	if (separator != NULL && i < n - 1)
 	{
-		printf("%s",separator);
+		printf("%s", separator);
 	}
 
 }
