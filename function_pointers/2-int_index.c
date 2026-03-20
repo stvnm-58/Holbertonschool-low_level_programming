1f1113d10e6a7b4333bd63d24bf421faea1b9090
steven <12992@holbertonstudents.com>
2026-03-20 12:05:07 +0100

    correction

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,9 +12,9 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 
-int i; 
+int i;
 
-if (array == NULL || cmp == NULL || size <= 0 )
+if (array == NULL || cmp == NULL || size <= 0)
 {
 	return (-1);
 }
@@ -25,7 +25,7 @@ for (i = 0; i < size; i++)
 	{
 	return (i);
 	}
-	
+
 }
 return (-1);
 
