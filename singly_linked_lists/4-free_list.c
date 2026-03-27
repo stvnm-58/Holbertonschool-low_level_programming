b5a2c74a7d33346fcb592fea5faace9e21e92c9b
steven <12992@holbertonstudents.com>
2026-03-27 09:54:27 +0100

    correction

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,24 +1,24 @@
+#include <stdlib.h>
 #include "lists.h"
 
-
-
-
+/**
+ * free_list - Libère une liste de type list_t.
+ * @head: Pointeur vers le début de la liste.
+ *
+ * Description: Libère chaque nœud de la liste, ainsi que
+ * la chaîne de caractères dupliquée à l'intérieur.
+ */
 void free_list(list_t *head)
 {
+	list_t *current;
+	list_t *next_node;
 
-list_t *current = head;
-list_t *next_node;
-
-
-while (current != NULL)
-{
-	next_node = current->next;
-
-	free(current->str);
-
-	free(current);
-
-	current = next_node;
-}
-
+	current = head;
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current->str);
+		free(current);
+		current = next_node;
+	}
 }
