346452fb5db148f54582b0751d7298c091995def
steven <12992@holbertonstudents.com>
2026-03-27 09:47:10 +0100

    correction

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,37 +1,35 @@
 #include "lists.h"
-#include <string.h>
-#include <stdlib.h>
+#include <string.h> // CORRECTION : Pour strdup
+#include <stdlib.h> // CORRECTION : Pour malloc et free
+
 /**
-* add_node - ajoute un nouveau nœud au début d'une liste list_t
-* @head: double pointeur vers la tête de la liste
-* @str: chaîne de caractères à dupliquer dans le nouveau nœud
-*
-* Return: l'adresse du nouvel élément, ou NULL en cas d'échec
-*/
+ * add_node - ajoute un nouveau nœud au début d'une liste list_t
+ * @head: double pointeur vers la tête de la liste
+ * @str: chaîne de caractères à dupliquer dans le nouveau nœud
+ *
+ * Return: l'adresse du nouvel élément, ou NULL en cas d'échec
+ */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node;
+	char *new_str;
+	unsigned int length = 0;
 
-list_t *new_node;
-char *new_str;
-unsigned int length = 0;
-
-if (str == NULL)
+	if (str == NULL)
 		return (NULL);
 
-new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 
-new_str = strdup(str);
+	new_str = strdup(str);
 	if (new_str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-while (str[length])
+	while (str[length])
 		length++;
 
 	new_node->str = new_str;
@@ -40,5 +38,5 @@ while (str[length])
 
 	*head = new_node;
 
-return (new_node);
+	return (new_node);
 }
