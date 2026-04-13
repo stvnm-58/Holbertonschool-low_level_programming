908f0bd59a91940e50d2e0f52c68161e89e027ad
steven <12992@holbertonstudents.com>
2026-04-13 19:09:43 +0200

    3-cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,64 +10,43 @@
  * @argv: Tableau des arguments.
  * Return: 0 en cas de succès.
  */
+
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
 	char buffer[1024];
 	ssize_t n_lu, n_ecrit;
-
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
+	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);}
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
 		exit(99);
 	}
-
 	while ((n_lu = read(fd_from, buffer, 1024)) > 0)
-	{
-		n_ecrit = write(fd_to, buffer, n_lu);
+	{	n_ecrit = write(fd_to, buffer, n_lu);
 		if (n_ecrit == -1 || n_ecrit != n_lu)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
-			exit(99);
-		}
-	}
-
+			exit(99);}}
 	if (n_lu == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
-		exit(98);
-	}
-
+		exit(98);}
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);}
 	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);}
 	return (0);
 }
